c571ee9a20c9225a60fb43fc8eaec6c54f66f86f
ringring <geekmango7@gmail.com>
2021-07-27 02:05:46 +0900

    Revert "Login.js / member.js 로그인기능 구현"
    This reverts commit 2957358f37d5a5ec55823177dae0621e9c8979ed.

--- back/app.js
+++ back/app.js
@@ -4,27 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//cors 노드 팩키지를 참조한다.
-const cors = require('cors');
-
 // .env 설정기능 참조 적용
 require('dotenv').config();
-
-// MVC(Model영역 모듈 참조) 시퀄라이즈 ORM 객체 참조하기
+//MVC(Model영역 모듈 참조) 시퀄라이즈 ORM 객체 참조하기
 var sequelize = require('./models/index.js').sequelize;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//사용자 정보 관리 라우팅 파일 참조
-var memberRouter = require('./routes/member');
-
 var app = express();
 
-//노드 어플리케이션에 cors기능 적용 - 모든 리소스 접근 CORS 허용하기
-app.use(cors());
-
-// 시퀄라이즈 ORM객체를 이용해 지정한 MySQL 연결 동기화하기
+//시퀄라이즈 ORM객체를 이용해 지정한 MySQL 연결 동기화하기
 sequelize.sync();
 
 // view engine setup
@@ -39,9 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// member 라우터 파일 기본 URL주소 정의
-app.use('/member', memberRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
